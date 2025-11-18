6de42f89a4c06b0db442ea7b602ee092d8e5e25e
Chris McComb <ccmcc2012@gmail.com>
2025-11-18 11:29:44 -0500

    Add oceans and Canadian provinces enums

--- src/geography.rs
+++ src/geography.rs
@@ -121,6 +121,56 @@ pub enum States {
     Wyoming,
 }
 
+/// The five major oceans of the world.
+#[derive(Debug, Copy, Clone)]
+#[cfg_attr(feature = "strum", derive(EnumIter, EnumCount))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum Ocean {
+    /// Arctic Ocean
+    Arctic,
+    /// Atlantic Ocean
+    Atlantic,
+    /// Indian Ocean
+    Indian,
+    /// Pacific Ocean
+    Pacific,
+    /// Southern Ocean
+    Southern,
+}
+
+/// Provinces and territories of Canada.
+#[derive(Debug, Copy, Clone)]
+#[cfg_attr(feature = "strum", derive(EnumIter, EnumCount))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum CanadianProvince {
+    /// Alberta
+    Alberta,
+    /// British Columbia
+    BritishColumbia,
+    /// Manitoba
+    Manitoba,
+    /// New Brunswick
+    NewBrunswick,
+    /// Newfoundland and Labrador
+    NewfoundlandAndLabrador,
+    /// Nova Scotia
+    NovaScotia,
+    /// Ontario
+    Ontario,
+    /// Prince Edward Island
+    PrinceEdwardIsland,
+    /// Quebec
+    Quebec,
+    /// Saskatchewan
+    Saskatchewan,
+    /// Northwest Territories
+    NorthwestTerritories,
+    /// Nunavut
+    Nunavut,
+    /// Yukon
+    Yukon,
+}
+
 #[cfg(test)]
 mod states_tests {
     use super::States;
@@ -138,6 +188,25 @@ mod states_tests {
         use strum::EnumCount;
 
         assert_eq!(<States as EnumCount>::COUNT, 50);
+    }
+}
+
+#[cfg(test)]
+mod geography_counts_tests {
+    #[cfg(feature = "enum-count")]
+    #[test]
+    fn oceans_have_five_variants() {
+        use strum::EnumCount;
+
+        assert_eq!(<super::Ocean as EnumCount>::COUNT, 5);
+    }
+
+    #[cfg(feature = "enum-count")]
+    #[test]
+    fn canadian_provinces_have_thirteen_variants() {
+        use strum::EnumCount;
+
+        assert_eq!(<super::CanadianProvince as EnumCount>::COUNT, 13);
     }
 }
 
